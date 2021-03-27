2d5794c9307da14cf840f7e8eacf0c2dd37fadc7
sunny mishra <sunny.mishra.rbl@gmail.com>
2021-03-27 23:54:23 +0530

    added two new algorithm and fixed some bugs

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var currentElement = 0;
 var originalInputArray = [];
-var SPEED = 0;
+var SPEED = 1;
 var selectedAlgorithm = "none";
 $(document).ready(function () {
   if (
@@ -17,7 +17,7 @@ $(document).ready(function () {
   });
   $("#speedCombo > .dropdown-menu > .dropdown-item").click((e) => {
     const speed = e.currentTarget.getAttribute("value");
-    SPEED = speed === "slow" ? 150 : speed === "medium" ? 50 : 10;
+    SPEED = speed === "slow" ? 50 : speed === "medium" ? 10 : 0.01;
     $("#speedCombo > #dropdownMenuLink").html(e.currentTarget.innerHTML);
   });
   $("#visualize").click((e) => {
@@ -40,9 +40,9 @@ randomArrayGenerator = () => {
       navigator.userAgent
     )
   ) {
-    size = 50;
+    size = 40;
   } else {
-    size = 150;
+    size = 100;
   }
   for (let i = 0; i < size; i++) {
     originalInputArray.push(Math.floor(Math.random() * 100));
@@ -58,9 +58,20 @@ visualize = () => {
     case "insertion":
       insertionSort(inputArr, SPEED);
       break;
-    case "bubble":
-      selectionSort(inputArr, SPEED);
+    case "quick":
+      quickSort(inputArr, 0, inputArr.length - 1);
       break;
+    case "bubble":
+      bubbleSort(inputArr, SPEED);
+      break;
+    case "counting":
+      countingSort(
+        inputArr,
+        inputArr.sort()[0],
+        inputArr.sort()[inputArr.length - 1]
+      );
+      break;
+
     default:
       alert("select any Algorithm first");
       $("#visualize").show();
@@ -78,7 +89,7 @@ render = (currentArray) => {
   } else {
     const nodes = $(".node");
     for (i in currentArray) {
-      nodes[i].style.height = currentArray[i] * 5 + "px";
+      nodes[i].style.height = currentArray[i] + "em";
       if (currentElement == i) {
         nodes[i].style.background = "blue";
       } else {
@@ -92,16 +103,23 @@ render = (currentArray) => {
 };
 bubbleSort = async (inputArr, speed) => {
   let len = inputArr.length;
+  let flag = true;
   for (let i = 0; i < len; i++) {
+    flag = false;
     for (let j = 0; j < len; j++) {
       if (inputArr[j] > inputArr[j + 1]) {
         let tmp = inputArr[j];
         inputArr[j] = inputArr[j + 1];
         inputArr[j + 1] = tmp;
-        await wait(speed);
-        currentElement = j;
-        render(inputArr);
+        flag = true;
       }
+      currentElement = j;
+      render(inputArr);
+      await wait(speed);
+      debugger;
+    }
+    if (!flag) {
+      break;
     }
   }
   $("#visualize").show();
@@ -117,13 +135,13 @@ selectionSort = async (arr, speed) => {
       if (arr[j] < arr[minIdx]) {
         minIdx = j;
       }
+      await wait(speed);
+      currentElement = j;
+      render(arr);
     }
     temp = arr[i];
     arr[i] = arr[minIdx];
     arr[minIdx] = temp;
-    await wait(speed);
-    render(arr);
-    currentElement = temp;
   }
   $("#visualize").show();
   $("#newArray").show();
@@ -158,3 +176,78 @@ wait = (ms) => {
     }, ms);
   });
 };
+
+//---------------------Quick Sort--------------------
+
+quickSort = async (items, left, right) => {
+  var index;
+  if (items.length > 1) {
+    index = await partition(items, left, right);
+    if (left < index - 1) {
+      quickSort(items, left, index - 1);
+    }
+    if (index < right) {
+      quickSort(items, index, right);
+    }
+  }
+  $("#visualize").show();
+  $("#newArray").show();
+};
+var items = [5, 3, 7, 6, 2, 9];
+function swap(items, leftIndex, rightIndex) {
+  var temp = items[leftIndex];
+  items[leftIndex] = items[rightIndex];
+  items[rightIndex] = temp;
+}
+partition = async (items, left, right) => {
+  var pivot = items[Math.floor((right + left) / 2)], //middle element
+    i = left, //left pointer
+    j = right; //right pointer
+  while (i <= j) {
+    while (items[i] < pivot) {
+      i++;
+    }
+    while (items[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      swap(items, i, j); //sawpping two elements
+      i++;
+      j--;
+    }
+    await wait(SPEED);
+    currentElement = i;
+    render(items);
+  }
+  return i;
+};
+
+//----------------------------------------------------------------
+
+//----------------------Counting Sort-----------------------------
+
+countingSort = async (arr, min, max) => {
+  let i = min,
+    j = 0,
+    len = arr.length,
+    count = [];
+  for (i; i <= max; i++) {
+    count[i] = 0;
+  }
+  for (i = 0; i < len; i++) {
+    count[arr[i]] += 1;
+  }
+  for (i = min; i <= max; i++) {
+    while (count[i] > 0) {
+      arr[j] = i;
+      j++;
+      count[i]--;
+    }
+    await wait(SPEED);
+    currentElement = j;
+    render(arr);
+  }
+  $("#visualize").show();
+  $("#newArray").show();
+};
+//-------------------------------------------------------------------
